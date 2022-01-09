bd13e29cf2bb34569a3fbd51645901d2b94c4dd4
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-01-09 19:41:54 -0300

    eventos submit e click

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,16 +1,21 @@
 <template>
     <div>
-    <form action = "">
+    <form action = "" @submit="sendForm($event)">
         <div>
-    <InputText/>
+            <input type = "text" v-model="name">
+    <!--<InputText/> -->
         </div>
-             <div>
-    <InputText/>
+        <div>
+            <input type = "text" v-model="email">
+    <!--<InputText/> -->
         </div>
     <div>
     <Submit/>
     </div>
-    </form>    
+    </form>
+    <p>Conteúdo</p>
+    <p>Conteúdo</p>    
+    <p>Conteúdo</p>        
     </div>
 </template>
 
@@ -23,7 +28,31 @@ export default{
     components:{
         InputText,
         Submit
+    }, 
+    data(){
+        return{
+            name: "",
+            email: ""
+        }
+    },
+    methods:{
+        sendForm(e){
+            e.preventDefault();
+            const name = this.name;
+            const email = this.email;
+            console.log("Formulario enviado!")
+            console.log("O nome é: " + name)
+            console.log("O email é: " + email)
+
+            //Ajax  - requisicao async com o servidor - Inserir no banco
+        }
     }
 }
 
 </script>
+
+<style scoped>
+    p{
+        margin: 150px;
+    }
+</style>
