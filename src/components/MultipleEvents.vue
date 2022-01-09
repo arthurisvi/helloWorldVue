96818bdef37ca3adbc2a158848d490d6dce955e3
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-01-09 20:19:15 -0300

    multiplos eventos

---
+++ src/components/MultipleEvents.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <button @click="first(txt1, $event), second(txt2, $event)">Ativar mútiplos eventos</button>
+    <p>{{ multipleEvents}}</p>
+</div>
+
+</template>
+
+<script>
+    export default{
+        name: "MultipleEvents",
+        data(){
+            return{
+                multipleEvents: "",
+                txt1: "Primeiro evento!",
+                txt2: "Segundo evento!"
+            }
+        },methods:{
+            first(txt, e){
+               this.multipleEvents = txt
+            },
+            second (txt, e){
+                setTimeout(()=>this.multipleEvents = txt, 500)
+            }
+
+        }
+    }
+
+</script>
+
+<style scoped>
+    div{
+        margin: 100px;
+    }
+
+</style>
--- src/components/firstComponent.vue
+++ src/components/firstComponent.vue
@@ -4,12 +4,18 @@
     Olá, Vue!
     </h1>
     <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+    <MultipleEvents/>
 </div>
 </template>
 
 <script>
+import MultipleEvents from './MultipleEvents.vue'
+
 export default{
   name: 'firstComponent',
+  components:{
+    MultipleEvents
+  },
   data(){
       return{
           nome: "Arthur",
